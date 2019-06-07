26a218995fad91cc390ade488e84f9a9a7461edd
vanessanaja <vanessacnaja@gmail.com>
2019-06-07 14:00:00 -0700

    add smallStraigth score

--- src/Rules.js
+++ src/Rules.js
@@ -67,7 +67,16 @@ class FullHouse extends Rule {
 /** Check for small straights. */
 
 class SmallStraight extends Rule {
-  // TODO
+  evalRoll = (dice) => {
+    const d = new Set(dice);
+    if(d.has(2) && d.has(3) && d.has(4) && d.has(1) || d.has(5)){
+      return this.score;
+    }
+    if(d.has(3) && d.has(4) && d.has(5) && d.has(2) || d.has(6)){
+      return this.score;
+    }
+    return 0;
+  }
 }
 
 /** Check for large straights. */
@@ -106,7 +115,7 @@ const fourOfKind = new SumDistro({ count: 4 });
 const fullHouse = new FullHouse({ score: 25 });
 
 // small/large straights score as 30/40
-const smallStraight = "TODO";
+const smallStraight = new SmallStraight( { score: 30 });
 const largeStraight = new LargeStraight({ score: 40 });
 
 // yahtzee scores as 50
