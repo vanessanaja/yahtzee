2310f651c269597ece95bb66927d2aacb217771a
vanessanaja <vanessacnaja@gmail.com>
2019-06-10 15:51:55 -0700

    fix rolling bug

--- src/Game.js
+++ src/Game.js
@@ -47,6 +47,7 @@ class Game extends Component {
 
   toggleLocked(idx) {
     // toggle whether idx is in locked or not
+    if(this.state.rollsLeft > 0){
     this.setState(st => ({
       locked: [
         ...st.locked.slice(0, idx),
@@ -54,6 +55,7 @@ class Game extends Component {
         ...st.locked.slice(idx + 1)
       ]
     }));
+    }
   }
 
   doScore(rulename, ruleFn) {
@@ -81,7 +83,7 @@ class Game extends Component {
             <div className='Game-button-wrapper'>
               <button
                 className='Game-reroll'
-                disabled={this.state.locked.every(x => x)}
+                disabled={this.state.locked.every(x => x) || this.state.rollsLeft === 0 }
                 onClick={this.roll}
               >
                 {this.state.rollsLeft} Rerolls Left
